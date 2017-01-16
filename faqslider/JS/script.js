b4ac9a8f9c2254a9ba1b5c294b18d25031c300dd
Hongyi Guo <guohongyi1991@gmail.com>
2017-01-16 16:03:50 -0500

    Done

--- faqslider/JS/script.js
+++ faqslider/JS/script.js
@@ -4,10 +4,19 @@ var speed = "500";
 
 //jQuery
 $(document).ready(function(){
+    //Question handler
     $('li.q').on(action, function(){
+        //Get next element
         $(this).next()
             .slideToggle(speed)
+                //Select all other answers
                 .siblings('li.a')
                     .slideUp();
+        //Get image for active question
+        var img = $(this).children('img');
+        //Remove the 'rotate' class for all images except the active
+        $('img').not(img).removeClass('rotate');
+        //Toggole rotate class
+        img.toggleClass('rotate');
     });
 });
